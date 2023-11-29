e4170c59e818f7f2971c31f08dae9ec8775774af
Demivdm <demi.van.der.maarl@hva.nl>
2023-11-29 11:34:54 +0100

    markdown parsed
    html not parsed

---
+++ i-love-web/src/components/markdown.js
@@ -0,0 +1,10 @@
+// // utils/markdown.js
+// export function parseMarkdown(markdown) {
+//   // van de hashtags een h2 maken
+//   markdown = markdown.replace(/## (.+)/g, "<h2>$1</h2>");
+
+//   // enter toevoegen
+//   markdown = markdown.replace(/(.+)\n/g, "$1<br>");
+
+//   return markdown;
+// }
